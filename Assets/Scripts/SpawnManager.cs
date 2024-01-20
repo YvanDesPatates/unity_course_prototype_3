0bddc073551d80bed21fb5fc8f6b927cb7d5e581
Yvan Roux <yvis99@gmail.com>
2024-01-20 23:39:00 +0100

    background infinit transition

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -9,8 +11,13 @@ public class SpawnManager : MonoBehaviour
     public float maxRangeSpawnInSec;
     
     private readonly Vector3 _spawnPos = new Vector3(25, 0, 0);
-    private float _lastSpawnTime = Time.time;
+    private float _lastSpawnTime;
     private float _spawnCoolDown = 0;
+
+    private void Start()
+    {
+        _lastSpawnTime = Time.time;
+    }
 
     // Update is called once per frame
     void Update()
