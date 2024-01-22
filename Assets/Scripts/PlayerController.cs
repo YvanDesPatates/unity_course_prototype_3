9d11b0089e5f1caf061e015ab457719dd7a3e879
Yvan Roux <yvis99@gmail.com>
2024-01-22 09:13:47 +0100

    sound effects !!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,14 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     
     private Rigidbody _playerRb;
     private bool _isOnGround = true;
     private bool _gameOver = false;
     private Animator _playerAnim;
+    private AudioSource _playerAudio;
     
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,7 @@ public class PlayerController : MonoBehaviour
         _playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         _playerAnim = GetComponent<Animator>();
+        _playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -32,6 +36,7 @@ public class PlayerController : MonoBehaviour
             _isOnGround = false;
             _playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            _playerAudio.PlayOneShot(jumpSound, 1);
         }   
     }
 
@@ -55,6 +60,7 @@ public class PlayerController : MonoBehaviour
         dirtParticle.Stop();
         _playerAnim.SetBool("Death_b", true);
         _playerAnim.SetInteger("DeathType_int", 1);
+        _playerAudio.PlayOneShot(crashSound, 1);
     }
 
     public bool IsGameOver()
